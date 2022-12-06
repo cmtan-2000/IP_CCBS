11413a5ae539de147d0ea10b5a948b8df113757c
Tan Chee Ming <cmtan0108@gmail.com>
2022-12-06 16:13:20 +0800

    First Commit

---
+++ IP_CCBS/WebContent/index.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+	<form name="loginForm" action="BasicController" method="get">
+	Full Name: <input type="text" name="fullname" placeholder="fullname"/><br>
+	Email: <input type="text" name="email" placeholder="email"/><br>
+	<input type="submit" value="submit">
+	</form>
+	
+</body>
+</html>
---
+++ IP_CCBS/src/controller/BasicController.java
@@ -0,0 +1,36 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class BasicController
+ */
+@WebServlet("/BasicController")
+public class BasicController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public BasicController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String fullname = request.getParameter("fullname");
+		String email = request.getParameter("email");
+		
+		PrintWriter pw = response.getWriter();
+		pw.println("<h3> Your name is " + fullname + "<h3>");
+		pw.println("<h3> Your email is " + email+ "<h3>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
